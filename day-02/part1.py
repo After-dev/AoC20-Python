9b642960f5dc44871665153ad02839991dc4586c
fpuentes <fpuentes@ujaen.es>
2020-12-06 12:09:53 +0100

    Day 2. Solution for parts 1 and 2 added

---
+++ day-02/part1.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def validate_passwords(passwords):
+	valid_passwords = 0
+
+	for entry in passwords:
+		[policy, password] = entry.split(":")
+		[policy_counts, policy_letter] = policy.split(" ")
+		[policy_min, policy_max] = [int(n) for n in policy_counts.split("-")]
+
+		count = password.count(policy_letter)
+		if policy_min <= count <= policy_max:
+			valid_passwords += 1
+
+	return valid_passwords
+
+
+
+# Example
+passwords = [
+	"1-3 a: abcde",
+	"1-3 b: cdefg",
+	"2-9 c: ccccccccc"
+]
+print(validate_passwords(passwords))
+
+# My data
+file = open('./input.data', 'r')
+passwords = [line[:-1] for line in file.readlines()]
+print(validate_passwords(passwords))
---
+++ day-02/part2.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def validate_passwords(passwords):
+	valid_passwords = 0
+
+	for entry in passwords:
+		[policy, password] = entry.split(":")
+		[policy_counts, policy_letter] = policy.split(" ")
+		[policy_min, policy_max] = [int(n) for n in policy_counts.split("-")]
+
+		if (password[policy_min] == policy_letter) != (password[policy_max] == policy_letter):
+			valid_passwords += 1
+
+	return valid_passwords
+
+
+
+# Example
+passwords = [
+	"1-3 a: abcde",
+	"1-3 b: cdefg",
+	"2-9 c: ccccccccc"
+]
+print(validate_passwords(passwords))
+
+# My data
+file = open('./input.data', 'r')
+passwords = [line[:-1] for line in file.readlines()]
+print(validate_passwords(passwords))
