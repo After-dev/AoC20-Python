25953de4b81195d4c1bcc3e0254dfab0682686e7
fpuentes <fpuentes@ujaen.es>
2020-12-15 12:42:02 +0100

    Day 15. Solution for parts 1 and 2 added

---
+++ day-15/part1.py
@@ -0,0 +1,55 @@
+import math
+import numpy as np
+
+def memory_game(starting_numbers):
+	# Initialize memory
+	memory = {}
+	for i,num in enumerate(starting_numbers):
+		memory[num] = i + 1
+
+	# Turns
+	next_number = len(memory) - memory[starting_numbers[-1]]
+	for turn in range(len(memory)+1, 2020):
+		current_number = next_number
+		next_number = turn - memory[current_number] if current_number in memory else 0
+		memory[current_number] = turn
+
+	return next_number
+
+
+
+
+# Example
+starting_numbers = """0,3,6"""
+starting_numbers_int = [int(n) for n in starting_numbers.split(",")]
+print(memory_game(starting_numbers_int))
+
+starting_numbers = """1,3,2"""
+starting_numbers_int = [int(n) for n in starting_numbers.split(",")]
+print(memory_game(starting_numbers_int))
+
+starting_numbers = """2,1,3"""
+starting_numbers_int = [int(n) for n in starting_numbers.split(",")]
+print(memory_game(starting_numbers_int))
+
+starting_numbers = """1,2,3"""
+starting_numbers_int = [int(n) for n in starting_numbers.split(",")]
+print(memory_game(starting_numbers_int))
+
+starting_numbers = """2,3,1"""
+starting_numbers_int = [int(n) for n in starting_numbers.split(",")]
+print(memory_game(starting_numbers_int))
+
+starting_numbers = """3,2,1"""
+starting_numbers_int = [int(n) for n in starting_numbers.split(",")]
+print(memory_game(starting_numbers_int))
+
+starting_numbers = """3,1,2"""
+starting_numbers_int = [int(n) for n in starting_numbers.split(",")]
+print(memory_game(starting_numbers_int))
+
+# My data
+print("Result for my puzzle:")
+file = open('./input.data', 'r')
+starting_numbers_int = [int(n) for n in file.read().split(",")]
+print(memory_game(starting_numbers_int))
